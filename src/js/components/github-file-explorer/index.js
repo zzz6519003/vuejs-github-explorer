7c591b2342d60c56ad5539b73e5fa3b60cba66e7
Snowmanzzz(Zhengzhong Zhao) <zhengzhongzhao@gmail.com>
2015-11-19 14:51:01 +0800

    biu

---
+++ src/js/components/github-file-explorer/index.js
@@ -0,0 +1,43 @@
+module.exports = {
+	template: require("./template.html"),
+	data: function () {
+		return {
+			path: '/',
+			files: []
+		};
+	},
+
+	props: {
+		username: {
+			type: String,
+			required: true
+		},
+
+		repo: {
+			type: String,
+			required: true
+		}
+	},
+
+	created: function () {
+		if (this.username && this.repo) {
+			this.getFiles()
+		}
+	},
+
+	methods: {
+		getFiles: function () {
+            this.$http.get('https://api.github.com/repos/' + 
+            	this.fullRepoUrl + '/contents' + this.path, 
+            	function (data) {
+            		this.files = data
+            	}
+		}
+	},
+
+	computed: {
+		fullRepoUrl: function() {
+			return this.username + '/' + this.repo
+		}
+	}
+}
